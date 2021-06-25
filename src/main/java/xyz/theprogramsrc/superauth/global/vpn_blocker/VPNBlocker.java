6f081ccbf7728f20bf747d65666a9794ff4c58fc
Francisco Solis <fran@theprogramsrc.xyz>
2021-06-25 10:27:35 -0400

    VPNBlocker Web APIv2 and Cache Fixes

--- src/main/java/xyz/theprogramsrc/superauth/global/vpn_blocker/VPNBlocker.java
+++ src/main/java/xyz/theprogramsrc/superauth/global/vpn_blocker/VPNBlocker.java
@@ -1,11 +1,12 @@
 package xyz.theprogramsrc.superauth.global.vpn_blocker;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import xyz.theprogramsrc.superauth.global.languages.LBase;
 import xyz.theprogramsrc.supercoreapi.SuperPlugin;
+import xyz.theprogramsrc.supercoreapi.global.networking.ConnectionBuilder;
+import xyz.theprogramsrc.supercoreapi.global.networking.CustomConnection;
 import xyz.theprogramsrc.supercoreapi.global.utils.Utils;
+import xyz.theprogramsrc.supercoreapi.libs.google.gson.JsonObject;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class VPNBlocker {
@@ -18,41 +19,37 @@ public class VPNBlocker {
     public VPNBlocker(SuperPlugin<?> plugin, boolean enabled){
         this.plugin = plugin;
         this.cache = new HashMap<>();
-        this.apiEndpoint = "https://api.theprogramsrc.xyz/superauth/ip_checker/?ip={IP}";
+        this.apiEndpoint = "https://api-v2.theprogramsrc.xyz/api/superauth/vpnip/check/{IP}";
         this.enabled = enabled;
     }
 
     public boolean isVPN(String ip){
         if(!this.enabled)
             return false;
-
         if(!Utils.isConnected())
             return false;
-
         if(ip == null)
             return false;
-
-        if(ip.equals("") || ip.equals(" ") || ip.equals("null") || this.cache.containsKey(ip))
+        if(ip.equals("") || ip.equals(" ") || ip.equals("null"))
             return false;
-
-        if(this.cache.size() > 50000)
+        if(this.cache.containsKey(ip))
+            return this.cache.get(ip);
+        if(this.cache.size() > 90000)
             this.cache.clear();
+
         String url = this.apiEndpoint.replace("{IP}", ip);
-        String content = Utils.readWithInputStream(url);
-        if(content == null) {
-            this.plugin.log("&c" + LBase.NULL_CONTENT_RETURNED);
-            return false;
+        try{
+            CustomConnection connection = ConnectionBuilder.connect(url);
+            if(connection.isValidResponse() && connection.isResponseNotNull()){
+                JsonObject json = connection.getResponseJson();
+                boolean check = json.get("data").getAsJsonObject().get("check").getAsBoolean();
+                this.cache.put(ip, check);
+                return check;
+            }
+        }catch (IOException e){
+            this.plugin.addError(e);
+            e.printStackTrace();
         }
-        JsonObject json = new JsonParser().parse(content).getAsJsonObject();
-        String code = json.get("code").getAsString();
-        if(!code.equals("200")){
-            this.plugin.log("&c" + LBase.SERVER_ERROR_WHILE_CHECKING_IP);
-            this.plugin.log("&c" + json.get("message").getAsString());
-            return false;
-        }else{
-            boolean value = json.get("message").getAsString().equals("true");
-            this.cache.put(ip, value);
-            return value;
-        }
+        return false;
     }
 }
